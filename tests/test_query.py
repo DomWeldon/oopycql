89c42bc332c71635f0f6df117352425ed4acff8e
Dom Weldon <dom.weldon@gmail.com>
2017-08-20 17:13:39 +0100

    encoding issue for python 2.7

--- tests/test_query.py
+++ tests/test_query.py
@@ -51,7 +51,7 @@ class RegexForParameterFindingTestCase(TestCase):
     def test_all_test_queries(self):
         for q in self.TEST_QUERIES:
             ps = CypherQuery.find_params_in_query(q.query)
-            print('Testing for {0}'.format(q.query))
+            print('Testing for {0}'.format(q.query.encode('utf8')))
             print(ps)
             assert len(ps) == len(q.params)
             assert ps == q.params
