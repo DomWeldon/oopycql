f7a6b40d14c20794549cc8aa09727a782604060c
Dom Weldon <dom.weldon@gmail.com>
2017-09-21 06:11:08 +0100

    Upping version number after couple of issues fixed

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ __author__ = 'Dom Weldon <dom.weldon@gmail.com>'
 __email__ = 'dom.weldon@gmail.com'
 __license__ = 'Apache License, Version 2.0'
 __package__ = 'oopycql'
-__version__ = '1.1.3a1'
+__version__ = '1.1.4a0'
 
 required_deps = [
     'six',
